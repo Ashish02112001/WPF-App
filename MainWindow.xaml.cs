ff8abeeb3ebaeec749961c356e1bd2d7ad12a73a
Baalamuruganas <AshishKumar.Baalamurugan@trumpf.com>
2024-02-21 12:12:00 +0530

    Scribble pad with saving and loading as text files

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,75 +3,60 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Win32;
+using System.IO;
 
 namespace WpfAppAssignments {
     public partial class MainWindow : Window {
-        bool isDrawing = false;
-        Point startPoint;
-        Pen pen = new (Brushes.White, 2);
-        PathFigure currentPathFigure;
-        PathGeometry pathGeometry;
-        Path path;
         public MainWindow () {
             InitializeComponent ();
-            mThick.Visibility = Visibility.Collapsed;
         }
-        private void Canvas_MouseDown (object sender, MouseButtonEventArgs e) {
-            if (e.ButtonState == MouseButtonState.Pressed) {
-                isDrawing = true;
-                startPoint = e.GetPosition (canvas);
-                currentPathFigure = new PathFigure { StartPoint = startPoint };
-                pathGeometry = new PathGeometry ();
-                pathGeometry.Figures.Add (currentPathFigure);
-
-                path = new Path {
-                    Stroke = pen.Brush,
-                    StrokeThickness = pen.Thickness,
-                    Data = pathGeometry
-                };
-                canvas.Children.Add (path);
-            }
-        }
-
-        private void Canvas_MouseMove (object sender, MouseEventArgs e) {
-            if (isDrawing && e.LeftButton == MouseButtonState.Pressed) {
-                Point currentPoint = e.GetPosition (canvas);
-                LineSegment segment = new LineSegment (currentPoint, true);
-                currentPathFigure.Segments.Add (segment);
-                mThick.Visibility=Visibility.Collapsed;
-            }
-        }
-
-        private void Canvas_MouseUp (object sender, MouseButtonEventArgs e) {
-            isDrawing = false;
-        }
-
-        private void mThick_ValueChanged (object sender, RoutedPropertyChangedEventArgs<double> e) {
-            pen.Thickness = e.NewValue;
-            mCircle.Height = mCircle.Width = pen.Thickness;
-            mNThick.Text = pen.Thickness.ToString ("N1");
-        }
-
-        private void mSelect_SelectionChanged (object sender, SelectionChangedEventArgs e) {
-            switch (mSelect.SelectedIndex) {
-                case 0: pen = new (Brushes.White, pen.Thickness); mThick.Background = Brushes.White; break;
-                case 1: pen = new (Brushes.Green, pen.Thickness); mThick.Background = Brushes.Green; break;
-                case 2: pen = new (Brushes.Orange, pen.Thickness); mThick.Background = Brushes.Orange; break;
-                case 3: pen = new (Brushes.Red, pen.Thickness); mThick.Background = Brushes.Red; break;
-                case 4: pen = new (Brushes.Yellow, pen.Thickness); mThick.Background = Brushes.Yellow; break;
-            }
-        }
-
         private void Button_Click (object sender, RoutedEventArgs e) {
             mThick.Visibility = Visibility.Visible;
         }
 
         private void Button_Click_Erase (object sender, RoutedEventArgs e) {
-            pen = new (Brushes.Black, pen.Thickness);
+            mScribble.mPen = new (Brushes.Black, mScribble.mPen.Thickness);
         }
 
         private void Button_Click_Clear (object sender, RoutedEventArgs e) {
-            canvas.Children.Clear ();
+            mScribble.Children.Clear ();
+        }
+        private void mSelect_SelectionChanged (object sender, SelectionChangedEventArgs e) {
+            SolidColorBrush color = Brushes.White;
+            switch (mSelect.SelectedIndex) {
+                case 0: color = Brushes.White; mThick.Background = Brushes.White; break;
+                case 1: color = Brushes.Green; mThick.Background = Brushes.Green; break;
+                case 2: color = Brushes.Orange; mThick.Background = Brushes.Orange; break;
+                case 3: color = Brushes.Red; mThick.Background = Brushes.Red; break;
+                case 4: color = Brushes.Yellow; mThick.Background = Brushes.Yellow; break;
+            }
+            mScribble.mPen.Brush = color;
+        }
+        private void mThick_ValueChanged (object sender, RoutedPropertyChangedEventArgs<double> e) {
+            mScribble.mPen.Thickness = e.NewValue;
+            mCircle.Height = mCircle.Width = mScribble.mPen.Thickness;
+            mNThick.Text = mScribble.mPen.Thickness.ToString ("N1");
+        }
+
+        private void Button_Click_Save (object sender, RoutedEventArgs e) {
+            SaveFileDialog saveFile = new ();
+            if (saveFile.ShowDialog () == true) {
+                StreamWriter sw = new (saveFile.FileName, true);
+                foreach (var points in ScribblePad.mWayPoints) sw.WriteLine (points);
+                sw.Close ();
+            }
+        }
+
+        private void Button_Click_Load (object sender, RoutedEventArgs e) {
+            OpenFileDialog openFileDialog = new ();
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog () is true) {
+                var fileName = openFileDialog.FileName;
+                mScribble.Load (fileName);
+            }
         }
     }
 }
---
+++ ScribbleClass.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Input;
+using System.Configuration;
+using System.IO;
+
+namespace WpfAppAssignments {
+    public partial class ScribblePad : Canvas {
+        bool isDrawing = false;
+        Point mStartPoint, mEndPoint;
+        static public List<Point> mWayPoints = new ();
+        public Pen mPen = new (Brushes.White, 2);
+        protected override void OnRender (DrawingContext drawingContext) {
+            base.OnRender (drawingContext);
+            if (mWayPoints.Count > 1) {
+                for (int i = 0; i < mWayPoints.Count - 1; i++) {
+                    if (mWayPoints[i] == mX) continue;
+                    var start = mWayPoints[i];
+                    var end = mWayPoints[i + 1];
+                    drawingContext.DrawLine (mPen, start, end);
+                }
+            }
+        }
+        public void Load (string fileName) {
+            string[] points = File.ReadAllLines (fileName);
+            foreach (var point in points) {
+                string[] pt = point.Split (',');
+                if (double.TryParse (pt[0], out double x) && double.TryParse (pt[1], out double y)) {
+                    mWayPoints.Add (new Point (x, y));
+                }
+            }
+            InvalidateVisual ();
+        }
+
+        protected override void OnMouseDown (MouseButtonEventArgs e) {
+            base.OnMouseDown (e);
+            if (e.ButtonState == MouseButtonState.Pressed) {
+                isDrawing = true;
+                mStartPoint = e.GetPosition (this);
+                mWayPoints.Add (mStartPoint);
+            }
+        }
+        protected override void OnMouseMove (MouseEventArgs e) {
+            base.OnMouseMove (e);
+            if (isDrawing && e.LeftButton == MouseButtonState.Pressed) {
+                mEndPoint = e.GetPosition (this);
+                mWayPoints.Add (mEndPoint);
+                InvalidateVisual ();
+            }
+        }
+
+        protected override void OnMouseUp (MouseButtonEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Released) {
+                isDrawing = false;
+            }
+            mWayPoints.Add (mX);
+        }
+        Point mX = new (double.NaN, double.NaN);
+    }
+}
