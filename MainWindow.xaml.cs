257188c3a57b1b753eafc2e59445aef1ef54ceca
Baalamuruganas <AshishKumar.Baalamurugan@trumpf.com>
2024-02-23 11:09:01 +0530

    Changed the icon positions to top and added save and load for bin files

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,55 +7,52 @@ using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Win32;
 using System.IO;
+using System.Text;
 
 namespace WpfAppAssignments {
     public partial class MainWindow : Window {
         public MainWindow () {
             InitializeComponent ();
         }
-        private void Button_Click (object sender, RoutedEventArgs e) {
-            mThick.Visibility = Visibility.Visible;
-        }
 
         private void Button_Click_Erase (object sender, RoutedEventArgs e) {
-            mScribble.mPen = new (Brushes.Black, mScribble.mPen.Thickness);
+            //mScribble.mPen = new (Brushes.Black, mScribble.mPen.Thickness);
         }
 
         private void Button_Click_Clear (object sender, RoutedEventArgs e) {
             mScribble.Children.Clear ();
         }
         private void mSelect_SelectionChanged (object sender, SelectionChangedEventArgs e) {
-            SolidColorBrush color = Brushes.White;
-            switch (mSelect.SelectedIndex) {
-                case 0: color = Brushes.White; mThick.Background = Brushes.White; break;
-                case 1: color = Brushes.Green; mThick.Background = Brushes.Green; break;
-                case 2: color = Brushes.Orange; mThick.Background = Brushes.Orange; break;
-                case 3: color = Brushes.Red; mThick.Background = Brushes.Red; break;
-                case 4: color = Brushes.Yellow; mThick.Background = Brushes.Yellow; break;
-            }
-            mScribble.mPen.Brush = color;
-        }
-        private void mThick_ValueChanged (object sender, RoutedPropertyChangedEventArgs<double> e) {
-            mScribble.mPen.Thickness = e.NewValue;
-            mCircle.Height = mCircle.Width = mScribble.mPen.Thickness;
-            mNThick.Text = mScribble.mPen.Thickness.ToString ("N1");
+            mScribble.ColorChange (mSelect.SelectedIndex);
         }
 
         private void Button_Click_Save (object sender, RoutedEventArgs e) {
             SaveFileDialog saveFile = new ();
+            saveFile.Filter = "Text files (*.txt)|*.txt|Binary files (*.bin)|*.bin";
             if (saveFile.ShowDialog () == true) {
-                StreamWriter sw = new (saveFile.FileName, true);
-                foreach (var points in ScribblePad.mWayPoints) sw.WriteLine (points);
-                sw.Close ();
+                var ext = System.IO.Path.GetExtension (saveFile.FileName);
+                if (ext is ".txt") {
+                    StreamWriter sw = new (saveFile.FileName, true);
+                    foreach (var points in ScribblePad.mWayPoints) sw.WriteLine (points);
+                    sw.Close ();
+                } else {
+                    BinaryWriter bw = new BinaryWriter (File.Open (saveFile.FileName, FileMode.Append));
+                    foreach (var points in ScribblePad.mWayPoints) {
+                        bw.Write (points.X); bw.Write (points.Y); 
+                    }
+                    bw.Write (double.NaN);
+                    bw.Close ();
+                }
             }
         }
 
         private void Button_Click_Load (object sender, RoutedEventArgs e) {
             OpenFileDialog openFileDialog = new ();
-            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|Binary files (*.bin)|*.bin";
             if (openFileDialog.ShowDialog () is true) {
                 var fileName = openFileDialog.FileName;
                 mScribble.Load (fileName);
+                //mScribble.Load (fileName);
             }
         }
     }
--- ScribbleClass.cs
+++ ScribbleClass.cs
@@ -12,10 +12,10 @@ using System.IO;
 
 namespace WpfAppAssignments {
     public partial class ScribblePad : Canvas {
-        bool isDrawing = false;
+        bool mIsDrawing = false;
+        SolidColorBrush mColor;
         Point mStartPoint, mEndPoint;
         static public List<Point> mWayPoints = new ();
-        public Pen mPen = new (Brushes.White, 2);
         protected override void OnRender (DrawingContext drawingContext) {
             base.OnRender (drawingContext);
             if (mWayPoints.Count > 1) {
@@ -23,32 +23,56 @@ namespace WpfAppAssignments {
                     if (mWayPoints[i] == mX) continue;
                     var start = mWayPoints[i];
                     var end = mWayPoints[i + 1];
-                    drawingContext.DrawLine (mPen, start, end);
+                    drawingContext.DrawLine (new Pen (mColor, 2), start, end);
                 }
             }
         }
         public void Load (string fileName) {
-            string[] points = File.ReadAllLines (fileName);
-            foreach (var point in points) {
-                string[] pt = point.Split (',');
-                if (double.TryParse (pt[0], out double x) && double.TryParse (pt[1], out double y)) {
-                    mWayPoints.Add (new Point (x, y));
+            string ext = Path.GetExtension (fileName);
+            if (ext is ".txt") {
+                string[] points = File.ReadAllLines (fileName);
+                foreach (var point in points) {
+                    string[] pt = point.Split (',');
+                    if (double.TryParse (pt[0], out double x) && double.TryParse (pt[1], out double y)) {
+                        mWayPoints.Add (new Point (x, y));
+                    }
+                }
+            } else {
+                using FileStream fs = new (fileName, FileMode.Open);
+                using BinaryReader br = new (fs);
+                while (br.BaseStream.Position < br.BaseStream.Length) {
+                    for (; ; ) {
+                        var X = br.ReadDouble ();
+                        if (double.IsNaN (X)) { mWayPoints.Add (new Point (double.NaN, double.NaN)); break; }
+                        var Y = br.ReadDouble ();
+                        mWayPoints.Add (new Point (X, Y));
+                    }
                 }
             }
             InvalidateVisual ();
         }
-
+        public void ColorChange (int choice) {
+            SolidColorBrush color = Brushes.White;
+            switch (choice) {
+                case 0: color = Brushes.White; break;
+                case 1: color = Brushes.Green; break;
+                case 2: color = Brushes.Orange; break;
+                case 3: color = Brushes.Red; break;
+                case 4: color = Brushes.Yellow; break;
+            }
+            mColor = color;
+        }
         protected override void OnMouseDown (MouseButtonEventArgs e) {
             base.OnMouseDown (e);
             if (e.ButtonState == MouseButtonState.Pressed) {
-                isDrawing = true;
+                mIsDrawing = true;
                 mStartPoint = e.GetPosition (this);
                 mWayPoints.Add (mStartPoint);
             }
         }
         protected override void OnMouseMove (MouseEventArgs e) {
             base.OnMouseMove (e);
-            if (isDrawing && e.LeftButton == MouseButtonState.Pressed) {
+            if (mIsDrawing && e.LeftButton == MouseButtonState.Pressed) {
                 mEndPoint = e.GetPosition (this);
                 mWayPoints.Add (mEndPoint);
                 InvalidateVisual ();
@@ -57,7 +81,7 @@ namespace WpfAppAssignments {
 
         protected override void OnMouseUp (MouseButtonEventArgs e) {
             if (e.LeftButton == MouseButtonState.Released) {
-                isDrawing = false;
+                mIsDrawing = false;
             }
             mWayPoints.Add (mX);
         }
